c2e1197536dd85167cf023f87d2eb73f798dd30a
Mallory-Busch <m.mallorybusch@gmail.com>
2015-11-05 17:10:19 +0100

    I used ids when I should have used classes...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,30 +23,30 @@
       //Show & hide toggle
       //CODE + DATA
         $('#info-show-cd').click(function(){
-            $('#item-text-cd').slideToggle(1000);
-            $('#item-cd').animate(1000);
+            $('.item-text-cd').slideToggle(1000);
+            $('.item-cd').animate(1000);
             $('#info-show-cd').hide(1000);
             $('#info-hide-cd').show(1000);
         });
 
         $('#info-hide-cd').click(function(){
-            $('#item-text-cd').slideToggle(1000);
-            $('#item-cd').animate(1000);
+            $('.item-text-cd').slideToggle(1000);
+            $('.item-cd').animate(1000);
             $('#info-show-cd').show(1000);
             $('#info-hide-cd').hide(1000);
         });
 
       //STRATEGY
         $('#info-show-st').click(function(){
-            $('#item-text-st').slideToggle(1000);
-            $('#item-st').animate(1000);
+            $('.item-text-st').slideToggle(1000);
+            $('.item-st').animate(1000);
             $('#info-show-st').hide(1000);
             $('#info-hide-st').show(1000);
         });
 
         $('#info-hide-st').click(function(){
-            $('#item-text-st').slideToggle(1000);
-            $('#item-st').animate(1000);
+            $('.item-text-st').slideToggle(1000);
+            $('.item-st').animate(1000);
             $('#info-show-st').show(1000);
             $('#info-hide-st').hide(1000);
         });
